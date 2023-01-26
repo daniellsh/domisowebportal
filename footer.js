8f138df95ba071da1d0daeeb1bc70171b35ac40c
Daniel Lau <s2059252@student.hkct.edu.hk>
2023-01-26 21:34:14 +0800

    clean up unnecessary resources

--- footer.js
+++
@@ -1,7 +0,0 @@
-fetch('footer.html')
-  .then(response => response.text())
-  .then(template => {
-    const compiledTemplate = Handlebars.compile(template);
-    const footerContainer = document.querySelector('.footer-container');
-    footerContainer.innerHTML = compiledTemplate();
-  });
--- navbar.js
+++
@@ -1,7 +0,0 @@
-fetch('navbar.html')
-  .then(response => response.text())
-  .then(template => {
-    const compiledTemplate = Handlebars.compile(template);
-    const navbarContainer = document.querySelector('.navbar-container');
-    navbarContainer.innerHTML = compiledTemplate();
-  });
