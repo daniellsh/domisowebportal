07b082edcb551311ab58bfef35a0e92fe31fbc21
Daniel Lau <s2059252@student.hkct.edu.hk>
2023-01-31 21:24:29 +0800

    add navbar header footer

---
+++ Footer.php
@@ -0,0 +1,16 @@
+<!-- Start Footer -->
+<footer class="bg-dark">
+  <div class="container text-white d-flex align-items-center justify-content-center py-3 text-center">
+    <span>Do Mi So Music & Art Studio 2023 | Tel: 2345-9876 | Email: support@domiso.edu.hk</span>
+  </div>
+</footer>
+<!-- End Footer -->
+<!-- Bootstrap Script -->
+<script src="./assets/js/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+<script src="./assets/js/owlcarousel/owl.carousel.min.js"></script>
+<!-- JQuery Script -->
+<script src="./assets/js/app.js"></script>
+</body>
+
+</html>
---
+++ Navbar.php
@@ -0,0 +1,75 @@
+<?php
+session_start();
+include './config/database.php';
+
+if (isset($_SESSION['user_id'])) {
+  $user_id = $_SESSION['user_id'];
+  $sql = "SELECT profile_image FROM users WHERE id = '$user_id'";
+  $result = mysqli_query($conn, $sql);
+  $user = mysqli_fetch_assoc($result);
+}
+
+?>
+<nav class="navbar center navbar-expand-lg bg-body-tertiary bg-dark bg-dark py-3" data-bs-theme="dark">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">
+      <img width="50" src="./assets/images/logo.png" />
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link <?= $nav == "home" ? "active" : "" ?>" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link <?= $nav == "about" ? "active" : "" ?>" aria-current="page" href="about.php">About us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link <?= $nav == "course" ? "active" : "" ?>" aria-current="page" href="course.php">Course</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link <?= $nav == "eshop" ? "active" : "" ?>" aria-current="page" href="eshop.php">eShop</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link <?= $nav == "contact" ? "active" : "" ?>" aria-current="page" href="contact.php">Contact</a>
+        </li>
+      </ul>
+      <?php
+      if (!isset($_SESSION['user_id'])) :
+      ?>
+        <div class="auth hidden">
+          <a href="./login.php" class="btn btn-link text-white">Login</a>
+          <a href="./register.php" class="btn btn-primary">Sign up</a>
+        </div>
+      <?php else : ?>
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <img src="<?= empty($user['profile_image']) ? "./assets/images/profile.png" : './uploads/profiles/' . $user['profile_image'] ?>" class="img-profile rounded-circle" alt="User Profile">
+            <?= $_SESSION['username'] ?>
+          </button>
+          <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+            <h4 class="dropdown-header"><?= ucfirst($_SESSION['type']) ?></h4>
+            <div class="dropdown-divider"></div>
+            <?php if (isset($_SESSION['type']) && $_SESSION['type'] == 'admin') : ?>
+              <a class="dropdown-item" href="./dashboard/index.php">Dashboard</a>
+            <?php endif; ?>
+            <?php if (isset($_SESSION['type']) && $_SESSION['type'] == 'teacher') : ?>
+              <a class="dropdown-item" href="./dashboard/courses.php">Courses</a>
+            <?php endif; ?>
+            <a class="dropdown-item" href="./inbox.php">Inbox</a>
+            <a class="dropdown-item" href="./profile.php">Profile</a>
+            <a class="dropdown-item" href="./my-course.php">My Course</a>
+            <a class="dropdown-item" href="./my-purchase.php">My Purchase</a>
+            <a class="dropdown-item" href="./my-drive.php">My Drive</a>
+            <a class="dropdown-item" href="./room-booking.php">Room Booking</a>
+            <a class="dropdown-item" href="./cart.php">My Cart</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="./logout.php">Logout</a>
+          </div>
+        </div>
+      <?php endif; ?>
+    </div>
+  </div>
+</nav>
