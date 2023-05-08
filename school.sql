c6c657cccc34899e541cb45fe4e4add37656f9db
Daniel Lau <s2059252@student.hkct.edu.hk>
2023-05-08 17:42:17 +0800

    update image assets

--- school.sql
+++ school.sql
@@ -26,6 +26,24 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `booking`
 --
+
+CREATE TABLE `booking` (
+  `booking_id` int(11) NOT NULL,
+  `booking_no` varchar(20) NOT NULL,
+  `room` varchar(50) NOT NULL,
+  `booking_date` datetime NOT NULL,
+  `booking_status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `booking`
+--
+
+INSERT INTO `booking` (`booking_id`, `booking_no`, `room`, `booking_date`, `booking_status`) VALUES
+(1, '590fa', 'saloon', '2023-04-22 16:38:39', 'Confirmed');
+
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `courses`
 --
@@ -59,6 +77,42 @@ INSERT INTO `courses` (`course_id`, `course_name`, `course_description`, `course
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `course_files`
+--
+
+CREATE TABLE `course_files` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `file_name` varchar(255) NOT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `course_files`
+--
+
+INSERT INTO `course_files` (`id`, `course_id`, `file_name`, `file_path`, `created_at`, `updated_at`) VALUES
+(1, 855, 'Web test 02.pdf', '1682195967_Web test 02.pdf', '2023-04-22 20:39:27', '2023-04-22 20:39:27'),
+(2, 493, 'pexels-pixabay-258154.jpg', '1682197296_pexels-pixabay-258154.jpg', '2023-04-22 21:01:36', '2023-04-22 21:01:36');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `drives`
+--
+
+CREATE TABLE `drives` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `course_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `enrollments`
 --
 
@@ -77,6 +131,55 @@ CREATE TABLE `enrollments` (
 
 INSERT INTO `enrollments` (`enrollment_id`, `user_id`, `course_id`, `enrollment_date`, `sessions`, `upcoming_date`) VALUES
 (1, 3, 855, '2023-04-22', 1, '2023-04-22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `map_data`
+--
+
+CREATE TABLE `map_data` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `latitude` decimal(10,8) NOT NULL,
+  `longitude` decimal(11,8) NOT NULL,
+  `google_api` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `address` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `message_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `message_text` text NOT NULL,
+  `sent_at` timestamp NULL DEFAULT current_timestamp(),
+  `sender_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `course_id` int(11) DEFAULT NULL,
+  `quantity` int(10) UNSIGNED NOT NULL,
+  `price` float(10,2) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -103,6 +206,39 @@ CREATE TABLE `products` (
 
 INSERT INTO `products` (`id`, `name`, `price`, `in_stock`, `description`, `thumbnail`, `image1`, `image2`, `image3`, `product_code`) VALUES
 (1, 'Pick', '15.0', 1, 'Pick Guitar', '6444469d20e0f.png', '', '', '', 'ba5a1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase`
+--
+
+CREATE TABLE `purchase` (
+  `id` int(11) NOT NULL,
+  `item_code` varchar(50) DEFAULT NULL,
+  `invoice_no` varchar(10) DEFAULT NULL,
+  `purchase_date` date DEFAULT NULL,
+  `item_name` varchar(255) DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `purchase`
+--
+
+INSERT INTO `purchase` (`id`, `item_code`, `invoice_no`, `purchase_date`, `item_name`, `status`, `user_id`) VALUES
+(1, 'ba5a1', '402b1', '2023-04-22', 'Pick', 'processing', 1),
+(2, 'ba5a1', '553f4', '2023-04-22', 'Pick', 'processing', 3),
+(3, '', '049c8', '2023-04-22', '', 'processing', 3),
+(4, 'ba5a1', '0bd2c', '2023-04-22', 'Pick', 'processing', 3),
+(5, '', '8b999', '2023-04-22', '', 'processing', 3),
+(6, '', 'a8dae', '2023-04-22', '', 'processing', 3),
+(7, '', '89242', '2023-04-24', '', 'processing', 3),
+(8, '', '1a22b', '2023-04-24', '', 'processing', 3),
+(9, '', '15c62', '2023-04-24', '', 'processing', 3),
+(10, 'ba5a1', 'b0d93', '2023-04-24', 'Pick', 'processing', 3),
+(11, '', '67d9d', '2023-04-24', '', 'processing', 3);
 
 -- --------------------------------------------------------
 
@@ -155,12 +291,49 @@ INSERT INTO `users` (`id`, `first_name`, `last_name`, `dateofbirth`, `gender`, `
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `user_booking`
+--
+
+CREATE TABLE `user_booking` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `booking_id` int(11) NOT NULL,
+  `booking_date` date DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`booking_id`);
+
+--
 -- Indexes for table `courses`
 --
 ALTER TABLE `courses`
   ADD PRIMARY KEY (`course_id`),
   ADD KEY `teacher_id` (`teacher_id`),
   ADD KEY `room_location` (`room_location`);
+
+--
+-- Indexes for table `course_files`
+--
+ALTER TABLE `course_files`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `course_id` (`course_id`);
+
+--
+-- Indexes for table `drives`
+--
+ALTER TABLE `drives`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `course_id` (`course_id`);
 
 --
 -- Indexes for table `enrollments`
@@ -171,9 +344,38 @@ ALTER TABLE `enrollments`
   ADD KEY `course_id` (`course_id`);
 
 --
+-- Indexes for table `map_data`
+--
+ALTER TABLE `map_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`message_id`),
+  ADD KEY `sender_id` (`sender_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `product_id_2` (`product_id`),
+  ADD KEY `course_id` (`course_id`);
+
+--
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `purchase`
+--
+ALTER TABLE `purchase`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -191,8 +393,34 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `user_booking`
+--
+ALTER TABLE `user_booking`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `booking_id` (`booking_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `course_files`
+--
+ALTER TABLE `course_files`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `drives`
+--
+ALTER TABLE `drives`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `enrollments`
@@ -201,10 +429,34 @@ ALTER TABLE `enrollments`
   MODIFY `enrollment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `map_data`
+--
+ALTER TABLE `map_data`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `purchase`
+--
+ALTER TABLE `purchase`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `site_content`
@@ -219,6 +471,12 @@ ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT for table `user_booking`
+--
+ALTER TABLE `user_booking`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- Constraints for dumped tables
 --
 
@@ -230,12 +488,45 @@ ALTER TABLE `courses`
   ADD CONSTRAINT `courses_ibfk_2` FOREIGN KEY (`room_location`) REFERENCES `booking` (`booking_id`);
 
 --
+-- Constraints for table `course_files`
+--
+ALTER TABLE `course_files`
+  ADD CONSTRAINT `course_files_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `drives`
+--
+ALTER TABLE `drives`
+  ADD CONSTRAINT `drives_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`) ON DELETE CASCADE;
+
+--
 -- Constraints for table `enrollments`
 --
 ALTER TABLE `enrollments`
   ADD CONSTRAINT `enrollments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `enrollments_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`);
 
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`);
+
+--
+-- Constraints for table `user_booking`
+--
+ALTER TABLE `user_booking`
+  ADD CONSTRAINT `user_booking_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `user_booking_ibfk_2` FOREIGN KEY (`booking_id`) REFERENCES `booking` (`booking_id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
